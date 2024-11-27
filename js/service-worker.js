eb06da3de494e6a01ae6a074619cf12f1e07c118
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-27 19:18:50 +0100

    16. change upload-btn icon. try to fix OFFLINE_ASSETS path

--- js/service-worker.js
+++ js/service-worker.js
@@ -3,23 +3,23 @@ const CACHE_NAME = "offline-cache-v1";
 
 // Resources to cache
 const OFFLINE_ASSETS = [
-  "/",
-  "/index.html",
-  "/manifest.json",
+  "./",
+  "./index.html",
+  "./manifest.json",
   // css
-  "/css/action-btn.css",
-  "/css/clock.css",
-  "/css/style.css",
+  "./css/action-btn.css",
+  "./css/clock.css",
+  "./css/style.css",
   // js
-  "/js/clock.js",
-  "/js/index.js",
-  "/js/utils.js"
+  "./js/clock.js",
+  "./js/index.js",
+  "./js/utils.js"
 ];
 
 // Install the service worker and cache assets
-self.addEventListener("install", event => {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then(function (cache) {
       console.log("Caching offline assets");
       return cache.addAll(OFFLINE_ASSETS);
     })
@@ -27,9 +27,9 @@ self.addEventListener("install", event => {
 });
 
 // Fetch resources from the cache when offline
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
